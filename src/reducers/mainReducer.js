3f5fa49fb7b5d8d9b291d280f29354bf8cd3eb47
Liza-T-XLS <shapi94@gmail.com>
2021-06-04 20:39:17 +0200

    Fix: when going back to the catalog, if selecting another quantity for a product that is already in the cart, would create a new entry with the same game, now just updates the quantity

--- src/reducers/mainReducer.js
+++ src/reducers/mainReducer.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-else-return */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-confusing-arrow */
 
@@ -22,20 +23,34 @@ const mainReducer = (state = initialState, action = {}) => {
         products: action.products,
       };
     case SAVE_PRODUCT_TO_CART: {
-      const newCartArray = [
-        ...state.cart,
-        {
-          productId: action.productId,
-          productImg: action.productImg,
-          productName: action.productName,
-          productQuantity: action.productQuantity,
-          productPrice: action.productPrice,
-          totalPrice: action.productQuantity * action.productPrice,
-        },
-      ];
-      return {
-        ...state,
-        cart: newCartArray,
+      const isProductInCart = state.cart.find((product) => product.productId === action.productId);
+      if (!isProductInCart) {
+        const newCartArray = [
+          ...state.cart,
+          {
+            productId: action.productId,
+            productImg: action.productImg,
+            productName: action.productName,
+            productQuantity: action.productQuantity,
+            productPrice: action.productPrice,
+            totalPrice: action.productQuantity * action.productPrice,
+          },
+        ];
+        return {
+          ...state,
+          cart: newCartArray,
+        };
+      } else {
+        const newCartArray = [...state.cart].map((product) =>
+          product.productId === action.productId ? {
+            ...product,
+            productQuantity: action.productQuantity,
+            totalPrice: action.productQuantity * action.productPrice,
+          } : product);
+        return {
+          ...state,
+          cart: newCartArray,
+        };
       };
     }
     case UPDATE_PRODUCT_QUANTITY: {
